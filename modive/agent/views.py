a3e9247f1629bac04db428ce41e0c0bb01f35188
ShinYEB <dpqls9799@naver.com>
2025-06-09 15:10:17 +0900

    [feat] 사용자 맞춤 서비스 구현

--- modive/agent/views.py
+++ modive/agent/views.py
@@ -18,6 +18,7 @@ class GenerateWeeklyFeedback(APIView):
 
         agent_response = invoke_agent_weekly_feedback(propmt)
         response = json.loads(agent_response)
+        response['answer_type'] = 'chat'
 
         return Response({'code':status.HTTP_200_OK, 'message':"OK", 'data':response})
 
@@ -26,14 +27,52 @@ class GenerateCustomReport(APIView):
 
         data = request.data
 
-        agent_response = invoke_agent_custom_report(data.get('prompt'), data.get('sessionId'))
+        agent_response, session_id = invoke_agent_custom_report(data.get('prompt'), data.get('session_id'))
 
-        return Response({'code':status.HTTP_200_OK, 'message':"OK", 'data':agent_response})
+        try:
+            parse_response = json.loads(agent_response)
+            response = parse_response
+            response['session_id'] = session_id
+        except:
+            response = {
+                "answer": agent_response,
+                "session_id": session_id,
+                "answer_type": "chat"
+            }
+
+        return Response({'code':status.HTTP_200_OK, 'message':"OK", 'data':response})
 
 class CreateSession(APIView):
     def post(self, request):
         data = request.data
 
         response = create_session(data.get('userId'))
+
+        return Response({'code':status.HTTP_200_OK, 'message':"OK", 'data':response})
+
+class Test(APIView):
+    def post(self, request):
+        temp = [
+            {
+                "type": "DailyStatsChart",
+                "props": [
+                  { "date": '2025-06-01', "userCount": 45, "driveCount": 120 },
+                  { "date": '2025-06-02', "userCount": 52, "driveCount": 135 },
+                  { "date": '2025-06-03', "userCount": 38, "driveCount": 98 },
+                  { "date": '2025-06-04', "userCount": 61, "driveCount": 156 },
+                  { "date": '2025-06-05', "userCount": 49, "driveCount": 142 },
+                  { "date": '2025-06-06', "userCount": 55, "driveCount": 167 },
+                  { "date": '2025-06-07', "userCount": 43, "driveCount": 134 },
+                  { "date": '2025-06-08', "userCount": 58, "driveCount": 149 }
+                ]
+            }
+        ]
+
+        response = {
+            "answer_type": "dashboard",
+            "session_id": "session_id",
+            "answer": "completion",
+            "components": temp,
+        }
 
         return Response({'code':status.HTTP_200_OK, 'message':"OK", 'data':response})
